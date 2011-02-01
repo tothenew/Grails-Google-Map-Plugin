92c926faff34514bc83e417a73323786a06352fe
Bhagwat Kumar <bhagwat@intelligrape.com>
2011-02-01 13:35:02 +0530

    changes to plugin descriptor

--- GoogleMapGrailsPlugin.groovy
+++ GoogleMapGrailsPlugin.groovy
@@ -6,12 +6,16 @@ class GoogleMapGrailsPlugin {
 	def version = "0.1"
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "1.3.6 > *"
-	// the other plugins this plugin depends on
-	def dependsOn = [:]
+
+	def dependsOn = ['jquery': '1.4.2 > *']
+
+	def loadAfter = ['jquery']
 	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
 			"grails-app/views/error.gsp",
-			"grails-app/views/index.gsp"
+			"grails-app/views/index.gsp",
+			"web-app/js/jquery/jquery-1.4.3.js",
+			"web-app/js/jquery/jquery-1.4.3.min.js"
 	]
 
 	def author = "Bhagwat Kumar"
